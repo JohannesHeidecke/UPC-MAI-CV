460a1902762fddc863b9eba78661ecf0d79b1456
Alejandro Suarez <asuarez@iri.upc.edu>
2016-11-14 17:38:45 +0100

    Some minor changes

--- Lab5/FD_ex2.m
+++ Lab5/FD_ex2.m
@@ -11,8 +11,11 @@ function FD_ex2()
 clc; close all; clear;
 
 %% Initialization
+
+N = 100;
+
 % Initialize the vector for storing the detection rate for each frame
-detection_rate = [];
+detection_rate = zeros(N,1);
 
 %% Detection over a video sequence (100 frames)
 
@@ -23,7 +26,7 @@ videoReader = VideoReader('Black_or_White_face_Morphing.ogv'); % substitute .ogv
 
 currAxes = axes;
 
-while hasFrame(videoReader)
+for idx = 1:N % Just 100 frames. Otherwhise : % while hasFrame(videoReader)
 
     % Extract the next video frame
     frame = readFrame(videoReader);
@@ -32,18 +35,22 @@ while hasFrame(videoReader)
     %bbox = step(faceDetector, frame); % uncomment this
     bbox = [350 250 500 300]; % comment this
     
+    detection_rate(idx) = size(bbox,1);
+    
     % Draw the returned bounding box around the detected face.
     
     image(frame, 'Parent', currAxes)
     
-    for idx = 1:size(bbox,1)
-        rectangle('Position', bbox(idx,:), 'EdgeColor', 'g')
+    for jdx = 1:size(bbox,1)
+        rectangle('Position', bbox(jdx,:), 'EdgeColor', 'g')
     end
     
-    %pause(1/videoReader.FrameRate)
+    pause(1/videoReader.FrameRate)
 end
 
+detection_percentage = sum(detection_rate)/N;
 
+display(['Faces has been detected during a ', num2str(detection_percentage*100), '% of the time'])
 
 end
 
